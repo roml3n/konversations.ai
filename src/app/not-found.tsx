13abe41f7a9036917dcc6dce271d2be75fbea9a5
Roman Lenjo <roman25lenjo@gmail.com>
2025-10-07 15:02:22 +0300

    Fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -37,84 +37,79 @@ export default function NotFound() {
       />
 
       {gameState !== "playing" && (
-        <section className="pointer-events-none w-full absolute top-[50%] border border-red-500 z-20 flex flex-col items-center gap-6 text-center">
-          <div className="w-full">
-            {gameState === "gameOver" ? (
-              <>
-                <h1 className="text-3xl font-semibold tracking-tight">
-                  Game Over!
-                </h1>
-                <div className="flex items-center gap-4">
-                  <Button
-                    href="#"
-                    label="RETRY"
-                    variant="solid"
-                    size="small"
-                    onClick={handleRetryClick}
-                  />
-                  <Button
-                    href="/"
-                    label="GO HOME"
-                    variant="outline"
-                    size="small"
-                  />
+        <>
+          <section className="pointer-events-none w-full absolute top-[50%] border border-red-500 z-20 flex flex-col items-center gap-6 text-center">
+            <div className="w-full">
+              {gameState === "gameOver" ? (
+                <>
+                  <h1 className="text-3xl font-semibold tracking-tight">
+                    Game Over!
+                  </h1>
+                  <div className="flex items-center gap-4">
+                    <Button
+                      href="#"
+                      label="RETRY"
+                      variant="solid"
+                      size="small"
+                      onClick={handleRetryClick}
+                    />
+                    <Button
+                      href="/"
+                      label="GO HOME"
+                      variant="outline"
+                      size="small"
+                    />
+                  </div>
+                </>
+              ) : (
+                <div className="flex flex-col items-center gap-6">
+                  <div className="flex flex-col items-center gap-3">
+                    <h1 className="text-3xl font-semibold tracking-tight">
+                      Uh-oh! Page not found.
+                    </h1>
+                    <p className="text-lg text-white/80">
+                      Defeat the enemies to get back home.
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Button
+                      href="#"
+                      label="START"
+                      variant="solid"
+                      size="small"
+                      onClick={handleStartClick}
+                    />
+                    <Button
+                      href="/"
+                      label="GO HOME"
+                      variant="outline"
+                      size="small"
+                    />
+                  </div>
                 </div>
-              </>
-            ) : (
-              <>
-                <h1 className="text-3xl font-semibold tracking-tight">
-                  Uh-oh! Page not found.
-                </h1>
-                <p className="text-lg text-white/80">
-                  Defeat the enemies to get back home.
-                </p>
-              </>
-            )}
-          </div>
+              )}
+            </div>
+          </section>
 
           {gameState === "initial" && (
-            <>
-              <div className="flex items-center gap-4">
-                <Button
-                  href="#"
-                  label="START"
-                  variant="solid"
-                  size="small"
-                  onClick={handleStartClick}
-                />
-                <Button
-                  href="/"
-                  label="GO HOME"
-                  variant="outline"
-                  size="small"
-                />
-              </div>
-
-              <div className="absolute bottom-0 pointer-events-auto mt-2 flex items-center gap-4">
-                <div className="flex flex-col items-center gap-3 text-white/80">
-                  <p className="text-lg">
-                    Press{" "}
-                    <kbd className="rounded border border-white/60 px-2 py-0.5">
-                      SPACE
-                    </kbd>{" "}
-                    to shoot. Hold for more power.
-                  </p>
-                  <p className="text-lg">
-                    Use{" "}
-                    <span className="rounded border border-white/60 px-1">
-                      ←
-                    </span>{" "}
-                    and{" "}
-                    <span className="rounded border border-white/60 px-1">
-                      →
-                    </span>{" "}
-                    to move.
-                  </p>
-                </div>
-              </div>
-            </>
+            <div className="absolute bottom-6 w-full pointer-events-auto mt-2 flex flex-col text-white/80 items-center gap-4">
+              <p className="text-lg">
+                Press{" "}
+                <kbd className="rounded border border-white/60 px-2 py-0.5">
+                  SPACE
+                </kbd>{" "}
+                to shoot. Hold for more power.
+              </p>
+              <p className="text-lg">
+                Use{" "}
+                <span className="rounded border border-white/60 px-1">←</span>{" "}
+                and{" "}
+                <span className="rounded border border-white/60 px-1">→</span>{" "}
+                to move.
+              </p>
+            </div>
           )}
-        </section>
+        </>
       )}
     </main>
   );
